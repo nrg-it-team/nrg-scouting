b403cdcbe97f213c17ec1979964e83fff1772675
Colin <hungrymamamia@gmail.com>
2023-10-13 18:44:11 -0700

    ^

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,9 @@ export default defineConfig({
     VitePWA({
       includeAssets: ["assets/*", "icons/*"],
       manifest: {
-        name: "Black Hawks Scouting",
+        name: "NRG Scouting",
         short_name: "Scouting",
-        description: "A scouting app by FRC Team 2834",
+        description: "A scouting app by FRC Team 948 (with assets from team 2834)",
         theme_color: "#292929",
         background_color: "#292929",
         icons: [
